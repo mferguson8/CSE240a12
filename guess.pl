6b3a5b871c98f83642403dcb3aa88b862b61ffd4
michael.ferguson <mferg3838@gmail.com>
2023-11-15 14:32:32 -0700

    added ceck value

--- guess.pl
+++ guess.pl
@@ -1,1 +1,12 @@
-random_between(1, 10, RandomNumber).
+random_between(1, 10, _RandomNumber).
+
+get_user_input :-
+    write('Enter a value: '),
+    read(UserInput),
+    check_value(UserInput).
+
+check_value(X) :-
+    (X > _RandomNumber ->
+        write('Higher');
+        write('Lower')
+    ).
