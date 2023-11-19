e94563863ba2d2a91bfb6fb23d784deee3b392c7
michael.ferguson <mferg3838@gmail.com>
2023-11-18 21:37:09 -0700

    now running

--- guess.pl
+++ guess.pl
@@ -1,12 +1,30 @@
-random_between(1, 10, _RandomNumber).
+:- use_module(library(random)).
 
-get_user_input :-
-    write('Enter a value: '),
-    read(UserInput),
-    check_value(UserInput).
+% Generate a random number between 1 and 10
+generate_random_number(Number) :-
+    random(1, 10, Number).
 
-check_value(X) :-
-    (X > _RandomNumber ->
-        write('Higher');
-        write('Lower')
-    ).
+% Predicate to play the guessing game
+guess :-
+    write('Try to guess a number between 1 and 10.'), nl,
+    generate_random_number(Target),
+    play_guessing_game(Target).
+
+% Helper predicate to handle the guessing logic
+play_guessing_game(Target) :-
+    write('|: '),
+    read(Guess),
+    check_guess(Guess, Target).
+
+% Check if the guess is correct, higher, or lower
+check_guess(Guess, Target) :-
+    Guess =:= Target,
+    write('Correct!'), nl, !.
+
+check_guess(Guess, Target) :-
+    Guess < Target,
+    write('Higher. '), play_guessing_game(Target).
+
+check_guess(Guess, Target) :-
+    Guess > Target,
+    write('Lower. '), play_guessing_game(Target).
